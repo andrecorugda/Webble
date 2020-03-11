f54065d2ed0a604b071dde380e1c1254195b937c
Andre Corugda <andre.corugda@shorecloud.com>
2020-03-11 11:10:52 +0800

    modified security .htaccess

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Author:
+ * Description: Simplified Native Web Development
+ * App: Webble
+ * Built For: Fun, Excitement and Boredome (Charooot!)
+ */
 require_once __DIR__.'/app/public/routes/Route.php';
-
-?>
