d20e0414ea75f7a149a1bb5d67146f05a785738f
Andre Corugda <andrecorugda1234@gmail.com>
2020-11-09 16:31:54 +0800

    dev branch

--- app/config/app.php
+++ app/config/app.php
@@ -5,13 +5,13 @@ class App
     public function getAppConfig()
     {
         return [
-            'app_root' => '/hotel',//App root defines root directory if existing,
+            'app_root' => '/development/webble',//App root defines root directory if existing,
             'app_name' => 'Kwarto App',//App name is used to define app name
             'app_title' => 'Kwarto App | ',// App title is used to define application title on tabs
             'app_icon' => '',
             'app_404' => '/404',
             'app_405' => '/405',
-            'app_index' => '/dashboard',//App index defines default uri directory
+            'app_index' => '/',//App index defines default uri directory
             'app_login_index' => '/login/user',//App login index defines default page
             'app_login_user' => '/login/user',//App login index defines default uri login user directory
             'app_login_host' => '/login/host',//App login index defines default uri login host directory
